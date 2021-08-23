f47162395cc665619240eeeb7c026dd6bc7ffc3a
lbrav <l.braverman.l@live.co.uk>
2021-08-23 14:45:37 +0100

    No innitial change

---
+++ src/test/java/com/messagebankapi/messagebankapi/MessageBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messagebankapi.messagebankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBankApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
