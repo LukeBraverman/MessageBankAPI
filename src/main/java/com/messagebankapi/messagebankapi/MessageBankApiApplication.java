4c71d1bd56ee84b26c16cf9b9cc40ac6b108797d
lbrav <l.braverman.l@live.co.uk>
2021-08-23 14:45:37 +0100

    No innitial change

---
+++ src/main/java/com/messagebankapi/messagebankapi/MessageBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.messagebankapi.messagebankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBankApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageBankApiApplication.class, args);
+    }
+
+}
